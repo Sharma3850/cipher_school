f1fbc8541555832d09a17e3cd565e9285bd1440a
Yogesh Sharma <72644648+Sharma3850@users.noreply.github.com>
2023-06-24 21:29:49 +0530

    Update TrappingTheRainwaterProblem.cpp

--- Lecture 6/TrappingTheRainwaterProblem.cpp
+++ Lecture 6/TrappingTheRainwaterProblem.cpp
@@ -1,41 +1,23 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-
+#include <bits/stdc++.h>
 using namespace std;
-
-class Solution {
-public:
-    int trap(vector<int>& h) {
-        int totWater = 0;
-        int left[h.size()];
-        int right[h.size()];
-        left[0] = h[0];
-        for (int i = 1; i < h.size(); i++) {
-            left[i] = max(left[i - 1], h[i]);
+int rainWater(vector<int> &heights){
+    int n = heights.size();
+        vector<int> lMax(n);
+        vector<int> rMax(n);
+        lMax[0] = heights[0];
+        rMax[n - 1] = heights[n - 1];
+        for(int i = 1; i < n; i++){
+            lMax[i] = max( heights[i], lMax[i - 1]);
+            rMax[n - i -1] = max( heights[n - i - 1], rMax[n - i]);
         }
-
-        right[h.size() - 1] = h[h.size() - 1];
-        for (int i = h.size() - 2; i >= 0; i--) {
-            right[i] = max(right[i + 1], h[i]);
-        }
-
-        for (int i = 0; i < h.size(); i++) {
-            int currWater = 0;
-            currWater = min(left[i], right[i]) - h[i];
-            totWater += currWater;
-        }
-        return totWater;
-    }
-};
-
-int main() {
-    Solution solution;
-    vector<int> heights = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};  
-
-    int trappedWater = solution.trap(heights);
-
-    cout << trappedWater << endl;
-
+        int water = 0;
+        for(int i = 1; i < n; i++){
+                    water += (min(lMax[i], rMax[i]) - heights[i]);
+                }
+                return water;
+}
+int main(){
+    vector<int> heights = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+    cout << rainWater(heights);
     return 0;
 }
